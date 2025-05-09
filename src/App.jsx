61294ff94d084bd64e3de2b7e793a11050538701
samWidlund <sam.wid415@gmail.com>
2025-05-09 12:14:06 +0200

    add random finWords functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import { useState } from 'react'
+import { use, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
+
+const getRandomItem = (finWords) => {
+  const randomIndex = Math.floor(Math.random() * finWords.length);
+  return finWords[randomIndex];
+}
 
 function compareInput(input) {
   const inputdata = input.toString().toLowerCase();
@@ -15,14 +21,28 @@ function compareInput(input) {
 function App() {
   const [input, setInput] = useState('')
   const [inputValue, setInputValue] = useState('')
+  const [randomItem, setRandomItem] = useState('')
+
+  const finWords = ['moi', 'hyvä', 'kiitos'];
+
+  const handleClick = () => {
+    setRandomItem(getRandomItem(finWords));
+  };
 
   return (
     <>
-    <div id="container">
+  <div id="container">
 
       <div id="mainHeader">
         <h1>moi!</h1>
       </div>
+      <button onClick={handleClick}>
+        Slumpa ord
+      </button>
+      <p>
+        Slumpat ord: {randomItem}
+      </p>
+
       <button onClick={() => setInput(inputValue)}>
           setName
       </button>
