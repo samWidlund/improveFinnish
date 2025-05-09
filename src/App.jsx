3e5a2524d1961ea4110f1c8cc59ffc7e87a927ee
samWidlund <sam.wid415@gmail.com>
2025-05-09 11:22:09 +0200

    add basic layer jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,44 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function compareInput(input) {
+  if (input == "test")
+  {
+    return("correct");
+  }
+}
+
 function App() {
   const [name, setName] = useState('')
   const [inputValue, setInputValue] = useState('')
+  const [returnValue , setReturnValue] = useState('')
+
   return (
     <>
-    <div className="card">
+    <div id="container">
+      <div id="mainHeader">
+        <h1>moi!</h1>
+      </div>
+      
+      <div>
       <button onClick={() => setName(inputValue)}>
         setName
       </button>
 
+      <button id="testing" onClick={compareInput(name)}>
+        click
+      </button>
+      <p>
+        is test true: {compareInput(name)}
+      </p>
+
+
       <input type="text" onChange={(e) => setInputValue(e.target.value)}></input>
       <p>
         name is: {name} 
-        </p>
+      </p>
       </div>
+    </div>
     </>
   )
 }
