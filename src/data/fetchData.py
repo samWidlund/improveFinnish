6d171d57aa79804910dc7344a62ca25527d14028
samWidlund <sam.wid415@gmail.com>
2025-05-19 13:11:02 +0200

    remove csv file and db python script

--- src/data/fetchData.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-import pandas as pd
-
-df = pd.read_csv("finnishWords.csv")
-conn = sqlite3.connect("source.db")
-
-df.to_sql("words", conn, if_exists="replace", index=False)
-
-conn.close()
