4c515f31bcdb5d2086dcf7a822f821b9f55abd3b
samWidlund <sam.wid415@gmail.com>
2025-05-20 07:57:45 +0200

    replace aios url with env variable

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   // fetch db data
   useEffect(() => {
-    axios.get('https://languageimprover-backend.onrender.com/data')
+    axios.get(`${import.meta.env.VITE_API_URL}/data`)
       .then((response) => {
         setData(response.data);
         console.log(response.data);
