33619ce3607de11aba16ac69e05411442b15333c
samWidlund <sam.wid415@gmail.com>
2025-05-23 08:47:59 +0200

    add working daisyUIgit diff

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,5 +2,6 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    // daisyUI behöver inga extra plugin inställningar här, den går via tailwindplugin
   },
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}", // viktiga filer där Tailwind ska kolla efter klasser
   ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light"], // Eller välj t.ex. ["dark", "cupcake"]
+  },
 }
