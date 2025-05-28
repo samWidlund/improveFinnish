a81d9037a28f618b26d77d0e1d0d0c03c091f625
samWidlund <sam.wid415@gmail.com>
2025-05-28 08:07:20 +0200

    add guest login alt

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -30,6 +30,7 @@ const LoginPage = () => (
                   <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                       Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
                   </p>
+                  <a href="#" className="block text-center font-light text-gray-500 dark:text-gray-400 hover:underline">Continue as guest</a>
               </form>
           </div>
       </div>
